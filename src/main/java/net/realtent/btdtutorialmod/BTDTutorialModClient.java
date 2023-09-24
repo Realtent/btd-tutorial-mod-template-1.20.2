f7fe2a31bcc574fc3be90bfa04c8f7f0de4a6165
Realtent <realtentwo@gmail.com>
2023-09-23 18:25:56 -0600

    Initial commit

---
+++ src/main/java/net/realtent/btdtutorialmod/BTDTutorialModClient.java
@@ -0,0 +1,10 @@
+package net.realtent.btdtutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BTDTutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/realtent/btdtutorialmod/BTDTutorialModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.realtent.btdtutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BTDTutorialModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
